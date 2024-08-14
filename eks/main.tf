4517bb9754fc7d54fdeb34de68547237d8f7772c
Your Name <you@example.com>
2024-08-15 02:10:46 +0900

    test

--- eks/main.tf
+++ eks/main.tf
@@ -23,7 +23,7 @@ module "min-eks" {
       min_size       = 1
       max_size       = 4
       desired_size   = 2
-      instance_types = ["t3.micro"]
+      instance_types = ["t3a.small"]
     }
   }
 
